a9e03e1e12095f366a3c146c9aea3bab33fdeabd
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-04-07 15:49:23 -0400

    Backend - Complete

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -4,11 +4,11 @@ import datetime
 db_connection = None
 
 def getSqlConnection():
-    global db_connection;
-    print("establishing connection to mysql db");
+    global db_connection
+    print("Establishing connection to mysql db")
 
     if db_connection is None:
         db_connection = mysql.connector.connect(user='root', password='root', database='gs');
-        print("db connected : ", db_connection);
+        print("db connected : ", db_connection)
 
-    return db_connection;
+    return db_connection
