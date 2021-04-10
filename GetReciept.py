b92405625e39c1da4e4c49e09776f7517216a4a5
Dewakar Agarwal <dewakar.agarwal@gmail.com>
2021-04-09 21:19:16 -0400

    GetReciept functional, Order summary generated.

---
+++ GetReciept.py
@@ -0,0 +1,98 @@
+import tkinter as tk
+from sqlite3 import dbapi2 as sqlite
+import sqlite3
+from tkinter import PhotoImage, filedialog, Text
+from tkinter.constants import END, NW
+from tkinter import Entry
+
+conn = sqlite3.connect("groceries.db")
+c = conn.cursor()
+
+
+def backToMain():
+    root.destroy()
+    # import GroceryAppMainPage
+    # backToMainPage = GroceryAppMainPage.openAgain()
+    openAgain()
+    c.close()
+
+
+def openAgain():
+    # make root visible again
+    root.iconify()
+    root.deiconify()
+
+
+# on submit button click
+def submit():
+    # if all text fields are filled
+    if itemNameField.index(("end")) != 0 and itemPriceField.index(("end")) != 0 and itemQTYfield.index(("end")) != 0:
+        userEntryItemName = itemNameField.get()
+        userEntryItemQTY = itemQTYfield.get()
+        userEntryItemPrice = itemPriceField.get()
+        print('lastrowid: ', insertNewRow(userEntryItemName, userEntryItemQTY, userEntryItemPrice))
+        sqlite_select_query = """SELECT * from groceries"""
+
+        c.execute(sqlite_select_query)
+        records = c.fetchall()
+        print("Total rows are:  ", len(records))
+        print("Printing each row")
+        for row in records:
+            print("Id: ", row[0])
+            print("Name: ", row[1])
+            print("Email: ", row[2])
+            print("\n")
+    else:
+        print("ONE FIELD IS EMPTY")
+
+
+def showOrderSummmary(canvas):
+    sqlite_select_query = """SELECT * from groceries"""
+    total = 0
+    c.execute(sqlite_select_query)
+    records = c.fetchall()
+    x = 30
+    y = 80
+    for row in records:
+        #print("Item: ", row[0], " | Quantity: ", row[1], " | Price: ", row[2])
+        rowData = "Item: " + row[0] + " |   Quantity: " + str(row[1]) + " |     Price: " + str(row[2])
+        rowDataString = str(rowData)
+        print(rowDataString)
+        itemNamelbl = tk.Label(canvas, text=(rowDataString),
+                               fg='black', font=("Helvetica", 11))
+        itemNamelbl.place(x=x, y=y)
+        y += 40
+        total += int(float(row[2]))
+
+    y += 40
+    x += 280
+    itemNamelbl = tk.Label(canvas, text=("Order Total : ", total), fg='red',
+                           font=("Helvetica", 14))
+    itemNamelbl.place(x=x, y=y)
+
+    print("Order Total : ", total)
+
+
+def insertNewRow(itemm_name, item_qty, price):
+    item_price = int(item_qty) * float(price)
+    sql_insert_statement = "INSERT INTO groceries (ITEM_NAME, ITEM_QUANTITY, ITEM_PRICE) values (?, ?, ?)"
+    data_tuple = (itemm_name, item_qty, item_price)
+    c.execute(sql_insert_statement, data_tuple)
+    conn.commit()
+    return c.lastrowid
+
+
+
+root = tk.Tk()
+canvas = tk.Canvas(root, heigh=680, width=500, bg="#263D42")
+showOrderSummmary(canvas)
+
+# Title Message To User Label
+lbl = tk.Label(canvas, text="Order Summary", fg='black', font=("Helvetica", 15, "bold"))
+lbl.place(x=180, y=30)
+
+backToMainButton = tk.Button(canvas, text="BACK TO MAIN MENU", height=2, width=30, command=backToMain)
+backToMainButton.place(x=95, y=625)
+
+canvas.pack()
+root.mainloop()
--- GroceryAppMainPage.py
+++ GroceryAppMainPage.py
@@ -1,116 +1,114 @@
 import sqlite3
-import tkinter as tk 
-from tkinter import PhotoImage, filedialog, Text
+import tkinter as tk
+from tkinter import PhotoImage, filedialog, Text, messagebox
 from tkinter.constants import END, NUMERIC, NW
 
 
 conn = sqlite3.connect("groceries.db")
 c = conn.cursor()
-#If table doesnt exist it creates it
+# If table doesnt exist it creates it
 try:
-   c.execute("""CREATE TABLE groceries (
+    c.execute("""CREATE TABLE groceries (
    ITEM_NAME TEXT,
    ITEM_QUANTITY INTEGER,
    ITEM_PRICE REAL
    )""")
 except:
-   #If table exits then do nothing all previous values for new order
-   print("Table Already Exits")
+    # If table exits then do nothing all previous values for new order
+    print("Table Already Exits")
 conn.commit()
 
-def newOrder():
-   setCurrentValue(0)
-   c.execute('DELETE FROM groceries;')
-   conn.commit()
 
-#method for setting Current Total Value
+def newOrder():
+    setCurrentValue(0)
+    c.execute('DELETE FROM groceries;')
+    conn.commit()
+
+
+# method for setting Current Total Value
 def setCurrentValue(newTotalPrice):
     currentTotalValue.set(str(newTotalPrice))
     return
 
+
 def openAgain():
-    #make root visible again
+    # make root visible again
     root.iconify()
     root.deiconify()
 
-#on addToOrder button click
-def addToOrder():  
-   root.destroy()
-   import AddToOrderPage
-   addOrderPage = AddToOrderPage.openAgain()
-   openAgain()
-   c.close()
+
+# on addToOrder button click
+def addToOrder():
+    root.destroy()
+    import AddToOrderPage
+    addOrderPage = AddToOrderPage.openAgain()
+    openAgain()
+    c.close()
 
 
-#on updateOrderButton button click
+# on updateOrderButton button click
 def updateOrder():
-   tk.messagebox.showinfo( "Add To Existing Order", "Order")
+    tk.messagebox.showinfo("Add To Existing Order", "Order")
 
-#on getBillButton button click
+
+# on getBillButton button click
 def getBill():
-   tk.messagebox.showinfo( "Add To Existing Order", "Order")
+    import GetReciept
+    getReciept = GetReciept.openAgain()
+    c.close()
+
 
 root = tk.Tk()
 root.title("GROCERY STORE BILL CALCULATOR")
 canvas1 = tk.Canvas(root, heigh=700, width=683, bg="#263D42")
 canvas1.pack()
 
-
-
-
 # #Welcome frame to add welcome Image to (the farme at the top)
 # welcomeFrame = tk.Frame(root, bg="white")
 # welcomeFrame.place(relwidth = 0.8, relheight = 0.3, relx=0.1, rely = 0.05)
 
 img = PhotoImage(file="./Images/WelcomeImage2.png")
-canvas1.create_image(20,20, anchor=NW, image=img)  
+canvas1.create_image(20, 20, anchor=NW, image=img)
 
-#Button Frames where all buttons are located
+# Button Frames where all buttons are located
 buttonFrame = tk.Frame(root, bg="gray")
-buttonFrame.place(relwidth = 0.8, relheight = 0.2, relx=0.1, rely = 0.65)
+buttonFrame.place(relwidth=0.8, relheight=0.2, relx=0.1, rely=0.65)
 
-#Button Frames where all buttons are located
+# Button Frames where all buttons are located
 currentTotalFrame = tk.Frame(root, bg="gray")
-currentTotalFrame.place(relwidth = 0.8, relheight = 0.1, relx=0.1, rely = 0.88)
+currentTotalFrame.place(relwidth=0.8, relheight=0.1, relx=0.1, rely=0.88)
 
-#"Current Total" Label
-lbl=tk.Label(currentTotalFrame, text="Current Total:", fg='red', font=("Helvetica", 14,"bold"))
+# "Current Total" Label
+lbl = tk.Label(currentTotalFrame, text="Current Total:", fg='red', font=("Helvetica", 14, "bold"))
 lbl.place(x=30, y=30)
 
-label = tk.Label(currentTotalFrame, text='$', fg="black", font=("Helvetica", 13,"bold"))
+label = tk.Label(currentTotalFrame, text='$', fg="black", font=("Helvetica", 13, "bold"))
 label.place(x=397, y=30)
 
-
-# "Current Total" TextField 
+# "Current Total" TextField
 currentTotalValue = tk.StringVar()
-currentTotalTxtFld=tk.Entry(currentTotalFrame, textvariable=currentTotalValue, bd=5)
+currentTotalTxtFld = tk.Entry(currentTotalFrame, textvariable=currentTotalValue, bd=5)
 currentTotalTxtFld.place(x=410, y=30)
 currentTotalTxtFld.configure(state="disabled")
 
-
-
-  
-
 c.execute("SELECT SUM(ITEM_PRICE) FROM groceries")
 [(totalPricesOfItems,)] = c.fetchall()
-#Set The price of the current item
-if(totalPricesOfItems == None):
-      setCurrentValue(0)
+# Set The price of the current item
+if (totalPricesOfItems == None):
+    setCurrentValue(0)
 else:
-   setCurrentValue(totalPricesOfItems)
+    setCurrentValue(totalPricesOfItems)
 
-newOrderButton = tk.Button(buttonFrame, text ="New Order?", height=2, width=20, command = newOrder)
+newOrderButton = tk.Button(buttonFrame, text="New Order?", height=2, width=20, command=newOrder)
 newOrderButton.place(x=200, y=20)
 
-addToOrderButton = tk.Button(buttonFrame, text ="Add To Order",  height=2, width=12,command = addToOrder)
+addToOrderButton = tk.Button(buttonFrame, text="Add To Order", height=2, width=12, command=addToOrder)
 addToOrderButton.place(x=30, y=80)
 
-updateOrderButton= tk.Button(buttonFrame, text ="Update Order", height=2, width=12, command = updateOrder)
+updateOrderButton = tk.Button(buttonFrame, text="Update Order", height=2, width=12, command=updateOrder)
 updateOrderButton.place(x=230, y=80)
 
-getBillButton = tk.Button(buttonFrame, text ="Get Receipt",  height=2, width=12,command = getBill)
+getBillButton = tk.Button(buttonFrame, text="Get Receipt", height=2, width=12, command=getBill)
 getBillButton.place(x=430, y=80)
-
-
 
 root.mainloop()
